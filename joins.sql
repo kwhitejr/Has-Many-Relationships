013c450a656b094c5fd4898e570a232d9d02f920
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-13 21:27:15 -1000

    first join query working

--- joins.sql
+++ joins.sql
@@ -1,10 +1,10 @@
-SELECT *
-FROM users;
+-- SELECT *
+-- FROM users;
 
-SELECT *
-FROM posts
-WHERE user_id = 100;
+-- SELECT *
+-- FROM posts
+-- WHERE user_id = 100;
 
-SELECT *, posts.user_id.first_name, posts.user_id.last_name
-FROM posts
+SELECT posts.*, users.first_name, users.last_name
+FROM posts INNER JOIN users ON (user_id = users.id)
 WHERE user_id = 200;
